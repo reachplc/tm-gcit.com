1481bb5654cd27d374e3f32864171a78a10b22b7
Michael Bragg <michael@michaelbragg.net>
2016-04-15 14:10:33 +0100

    Theme: Fix footer clearing

--- html/app/themes/gcit/footer-front-page.php
+++ html/app/themes/gcit/footer-front-page.php
@@ -7,12 +7,14 @@
  * @since GCIT 1.0
  */
 ?>
-<footer class="wrapper">
-	<div class="row">
-		<div class="large-3 columns"><img src="<?php echo esc_url( get_template_directory_uri() . '/images/gcit_logo.png' ); ?>" class="footer"/>
-		</div>
-		<div class="copyright large-9 columns text-right m-tb">
-			<span class="grey-mid-txt">Copyright &copy; Trinity Mirror plc <?php the_date( 'Y' ) ?> | Website by Trinity Mirror Creative</span>
+<footer style="clear: both">
+	<div class="wrapper">
+		<div class="row">
+			<div class="large-3 columns"><img src="<?php echo esc_url( get_template_directory_uri() . '/images/gcit_logo.png' ); ?>" class="footer"/>
+			</div>
+			<div class="copyright large-9 columns text-right m-tb">
+				<span class="grey-mid-txt">Copyright &copy; Trinity Mirror plc <?php the_date( 'Y' ) ?> | Website by Trinity Mirror Creative</span>
+			</div>
 		</div>
 	</div>
 </footer>
