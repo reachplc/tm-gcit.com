3b21b2ad5fd03aa68d593c8be0a700371e3e8d52
Rebecca Shipley <rebeccashipley@hw020909.ad1.tm-gnet.com>
2016-04-15 12:00:00 +0100

    no centred page template

---
+++ html/app/themes/gcit/page-nomenu-centred.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Template Name: Page - No Side Menu - Centred
+*/
+?>
+
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that other
+ * 'pages' on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage GCIT
+ * @since GCIT 1.0
+ */
+?>
+  <?php get_header(); ?>
+  <div class="row z-fix" >
+    <div class="large-12 columns">
+     
+    </div>
+  </div>
+</div>
+
+  <main id="primary" class="content-area">
+   
+    <?php /* The loop */ ?>
+      <?php while ( have_posts() ) : the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <div class="row black-bk">
+            <header class="entry-header wrapper">
+              <?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+              <div class="entry-thumbnail">
+              <?php the_post_thumbnail(); ?>
+              </div>
+              <?php endif; ?>
+
+              <h1 class="entry-title"><?php the_title(); ?></h1>
+            </header><!-- .entry-header -->
+          </div>
+
+
+          <div class="entry-content">
+
+                  
+                                  <div class="content-fillcenter">
+
+                                          <?php the_content(); ?>
+
+                                            <?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+
+
+                                   </div>
+                    
+
+
+
+
+
+
+            
+  
+          </div><!-- .entry-content -->
+
+
+      <?php endwhile; ?>
+  </main><!-- #primary -->
+
+
+<?php get_footer(); ?>
