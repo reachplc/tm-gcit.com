f6679cfa54738f8092911db2c19fd353fc38b2bd
Jon Masters <Jonathan_masters77@yahoo.co.uk>
2016-04-15 08:14:29 +0100

    Alignment changes to page-writer.php file

--- html/app/themes/gcit/page-writer.php
+++ html/app/themes/gcit/page-writer.php
@@ -45,10 +45,10 @@ Template Name: Page - Writer
 
 
           <div class="entry-content">
-
-                  <div class="large-5 left" style="display: flex; background-color: #333;">
+          
+               <div class ="row flex">
+                  <div class="large-5 left grey-dark ">
                       <div class="innermenu">
-                            
                             <div class="icon-pointer">
                               <a href="<?php echo esc_url( home_url( '/contentwatch/regional-resources/writer' ) ); ?>">
                               <img class=" size-full wp-image-255 alignnone" style="padding-top: 1px;" src="<?php echo esc_url( get_template_directory_uri() . '/images/iconpointer_right_writer.png' ); ?>" alt="Writer" width="222" height="142" />
@@ -58,36 +58,21 @@ Template Name: Page - Writer
                               <img class="icon-pointer-img" src="<?php echo esc_url( get_template_directory_uri() . '/images/iconpointer_production.png' ); ?>" alt="Production" />
                               </a>
                             </div>
-
                             <div class="secondary-nav-writer">
                             <?php wp_nav_menu( array( 'theme_location' => 'writer-menu' ) ); ?>
-                              
                             </div>
                       </div>
                 </div>
-
-
-
                   <div class="large-7 right">
                                   <div class="content-fill">
                                         <div class="body-content">
-
                                           <?php the_content(); ?>
-
                                             <?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
-
-
-                                        </div>
-                                   </div>
-                          </div>
-
-
-
-
-
-
-            
-  
+                                  </div>
+                              </div>
+                    </div>
+              </div>
+      
           </div><!-- .entry-content -->
 
 
