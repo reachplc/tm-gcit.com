141b2900fd538d555329a1a73a73a1bcaf4d830c
Michael Bragg <michael@michaelbragg.net>
2017-02-16 15:08:08 +0000

    Fix code standards with `comments.php`

--- html/app/themes/gcit/comments.php
+++ html/app/themes/gcit/comments.php
@@ -21,14 +21,22 @@ if ( post_password_required() ) {
 
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
-			<?php
-				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', '_s' ) ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			?>
-		</h2>
+		<?php $comment_count = get_comments_number(); ?>
+		<?php if ( 1 === $comment_count ) {
+			printf(
+				/* translators: 1: title. */
+				esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', '_s' ),
+				'<span>' . get_the_title() . '</span>'
+			);
+} else {
+	printf( // WPCS: XSS OK.
+		/* translators: 1: comment count number, 2: title. */
+		esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', '_s' ) ),
+		number_format_i18n( $comment_count ),
+		'<span>' . get_the_title() . '</span>'
+	);
+}	?>
+		</h2><!-- .comments-title -->
 
 		<ol class="comment-list">
 			<?php
